d665b9ea99f70e56050d9d7ec7d8c3c23614a700
lucafratipietro <luca.fratipietro@dawsoncollege.qc.ca>
2023-11-01 21:53:11 -0400

    Fix ESLINT errors in tests

--- server/__tests__/routes/app.test.js
+++ server/__tests__/routes/app.test.js
@@ -1,3 +1,7 @@
+// Disabled due to field in song object being called after that name
+// Can change in the future
+/* eslint-disable camelcase */
+
 import request from 'supertest';
 import { jest } from '@jest/globals';
 import app from '../../app.js';
@@ -5,28 +9,20 @@ import { DB } from '../../db/db.js';
 
 jest.mock('../../db/db');
 
-//mocking of the songs routes
-
-const songsMockDB = [
-  { Genre : 'rock', Title : 'The first Song', Artist : 'The Who'},
-  { Genre : 'pop', Title : 'The second', Artist : 'The Strokes'},
-  { Genre : 'rap', Title : 'Sleep', Artist : 'GodSpeed you!'}
-];
-
 //Get all songs
 describe('GET /songs', () => {
   test('It should respond with all songs json array', async () => {
     jest.spyOn(DB.prototype, 'getAllSongs').mockResolvedValue(
       [{ Genre : 'rock', Title : 'The first Song', Artist : 'The Who'},
-    { Genre : 'pop', Title : 'The second', Artist : 'The Strokes'},
-    { Genre : 'rap', Title : 'Sleep', Artist : 'GodSpeed you!'}]
+        { Genre : 'pop', Title : 'The second', Artist : 'The Strokes'},
+        { Genre : 'rap', Title : 'Sleep', Artist : 'GodSpeed you!'}]
     );
 
     const response = await request(app).get('/songs');
     expect(response.body).toEqual(
-    [{ Genre : 'rock', Title : 'The first Song', Artist : 'The Who'},
-    { Genre : 'pop', Title : 'The second', Artist : 'The Strokes'},
-    { Genre : 'rap', Title : 'Sleep', Artist : 'GodSpeed you!'}]
+      [{ Genre : 'rock', Title : 'The first Song', Artist : 'The Who'},
+        { Genre : 'pop', Title : 'The second', Artist : 'The Strokes'},
+        { Genre : 'rap', Title : 'Sleep', Artist : 'GodSpeed you!'}]
     );
     expect(response.statusCode).toBe(200);
     expect(response.type).toEqual('application/json');
@@ -38,15 +34,15 @@ describe('GET /songs', () => {
 describe('GET /songs', () => {
   test('Respond all objects with release_date being 2002', async () => {
     jest.spyOn(DB.prototype, 'getAllSongs').mockResolvedValue(
-      [{ Genre : 'rock', Title : 'The first Song', release_date : "2002" },
-      { Genre : 'pop', Title : 'The second', release_date : "1998"},
-      { Genre : 'rap', Title : 'Sleep', release_date : "2002-11-11"}]
+      [{ Genre : 'rock', Title : 'The first Song', release_date : '2002' },
+        { Genre : 'pop', Title : 'The second', release_date : '1998'},
+        { Genre : 'rap', Title : 'Sleep', release_date : '2002-11-11'}]
     );
 
     const response = await request(app).get('/songs?year=2002');
     expect(response.body).toEqual(
-      [{ Genre : 'rock', Title : 'The first Song', release_date : "2002" },
-      { Genre : 'rap', Title : 'Sleep', release_date : "2002-11-11"}]
+      [{ Genre : 'rock', Title : 'The first Song', release_date : '2002' },
+        { Genre : 'rap', Title : 'Sleep', release_date : '2002-11-11'}]
     );
     expect(response.statusCode).toBe(200);
     expect(response.type).toEqual('application/json');
