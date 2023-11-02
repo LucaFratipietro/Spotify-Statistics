c6a4692993e68ea4e421105f0f6d6c994402d280
lucafratipietro <luca.fratipietro@dawsoncollege.qc.ca>
2023-11-01 20:58:50 -0400

    Add commented out test for /songs/genre route

--- server/__tests__/app.test.js
+++ server/__tests__/app.test.js
@@ -7,19 +7,83 @@ jest.mock('../db/db.js');
 
 //mocking of the songs routes
 
-const songsMockDB = [{ Genre : 'rock', Title : 'The first Song', Artist : 'The Who'},
-  { Genre : 'pop', Title : 'The second', Artist : 'The Strokes'},
-  { Genre : 'rap', Title : 'Sleep', Artist : 'GodSpeed you!'}];
-
 //Get all songs
 describe('GET /songs', () => {
   test('It should respond with all songs json array', async () => {
-    jest.spyOn(DB.prototype, 'getAllSongs').mockResolvedValue(songsMockDB);
+    jest.spyOn(DB.prototype, 'getAllSongs').mockResolvedValue(
+      [{ Genre : 'rock', Title : 'The first Song', Artist : 'The Who'},
+    { Genre : 'pop', Title : 'The second', Artist : 'The Strokes'},
+    { Genre : 'rap', Title : 'Sleep', Artist : 'GodSpeed you!'}]
+    );
 
     const response = await request(app).get('/songs');
-    expect(response.body).toEqual(songsMockDB);
+    expect(response.body).toEqual(
+    [{ Genre : 'rock', Title : 'The first Song', Artist : 'The Who'},
+    { Genre : 'pop', Title : 'The second', Artist : 'The Strokes'},
+    { Genre : 'rap', Title : 'Sleep', Artist : 'GodSpeed you!'}]
+    );
     expect(response.statusCode).toBe(200);
     expect(response.type).toEqual('application/json');
 
   });
 });
+
+//Get all songs -- year query 
+describe('GET /songs', () => {
+  test('Respond all objects with release_date being 2002', async () => {
+    jest.spyOn(DB.prototype, 'getAllSongs').mockResolvedValue(
+      [{ Genre : 'rock', Title : 'The first Song', release_date : "2002" },
+      { Genre : 'pop', Title : 'The second', release_date : "1998"},
+      { Genre : 'rap', Title : 'Sleep', release_date : "2002-11-11"}]
+    );
+
+    const response = await request(app).get('/songs?year=2002');
+    expect(response.body).toEqual(
+      [{ Genre : 'rock', Title : 'The first Song', release_date : "2002" },
+      { Genre : 'rap', Title : 'Sleep', release_date : "2002-11-11"}]
+    );
+    expect(response.statusCode).toBe(200);
+    expect(response.type).toEqual('application/json');
+
+  });
+});
+
+//Get all songs by genre -- WORK IN PROGESS, BUG
+
+/*
+
+describe('GET /songs/:genre?year=invalid', () => {
+  test('Year returns no number, should be 404', async () => {
+    jest.spyOn(DB.prototype, 'getAllSongs', ).mockResolvedValue(
+      [{ Genre : 'rock', Title : 'The first Song', release_date : "2002" },
+      { Genre : 'pop', Title : 'The second', release_date : "1998"},
+      { Genre : 'rap', Title : 'Sleep', release_date : "2002-11-11"}]
+    );
+
+    const response = await request(app).get('/songs/rock');
+    expect(response.body).toEqual(
+      [{ Genre : 'rock', Title : 'The first Song', release_date : "2002" }]
+    );
+    expect(response.statusCode).toBe(200);
+    expect(response.type).toEqual('application/json');
+
+  });
+});
+
+describe('GET /songs/:genre', () => {
+  test('Respond all objects with Genre = rock', async () => {
+    jest.spyOn(DB.prototype, 'getAllSongs', ).mockResolvedValue(
+      [{ Genre : 'rock', Title : 'The first Song', release_date : "2002" },
+      { Genre : 'pop', Title : 'The second', release_date : "1998"},
+      { Genre : 'rap', Title : 'Sleep', release_date : "2002-11-11"}]
+    );
+
+    const response = await request(app).get('/songs/rnb');
+    expect(response.body).toEqual(
+      [{}]
+    );
+    expect(response.statusCode).toBe(404);
+    expect(response.type).toEqual('application/json');
+
+  });
+}); */
