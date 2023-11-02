45173e26a88925accaf82ee48cd155bb3fbd8a9e
Adriano D'Alonzo <adrianodalonzo@gmail.com>
2023-11-01 20:59:47 -0400

    Add 2nd unit test to test empty file returns nothing

--- server/__tests__/seed/app.test.js
+++ server/__tests__/seed/app.test.js
@@ -9,3 +9,11 @@ describe('Testing loading module function', () => {
     expect(dataFromFile[0].release_date).toEqual('1971-08-14');
   });
 });
+
+describe('Testing loading module function', () => {
+  test('Should return no values', async () => {
+    const dataFromFile = await readDataFromFile('data/empty_data.xlsx');
+    expect(dataFromFile).not.toBeNull();
+    expect(dataFromFile[0]).toBeUndefined();
+  });
+});
