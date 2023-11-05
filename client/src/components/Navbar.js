64d1a4ff95dc3cd76868eba1daee7377b5406a40
lucafratipietro <luca.fratipietro@dawsoncollege.qc.ca>
2023-11-05 15:50:23 -0500

    Add search functionality to navbar

--- client/src/components/Navbar.js
+++ client/src/components/Navbar.js
@@ -2,18 +2,29 @@ import '../styling/Navbar.css';
 
 export default function NavBar({handler}) {
 
-  function handleSubmuit(e){
-    
-  }
+  //handle submission of form
+  const handleSubmit = (e) => {
+    e.preventDefault();
+
+    //retreive data from form
+    const form = e.target;
+    const formData = new FormData(form);
+
+    const formJson = Object.fromEntries(formData.entries());
+    console.log(formJson);
+
+    //use handler to update genre and decade
+    handler(formJson.genre, formJson.decade);
+  };
 
   return (
     <nav>
       <p>
       Spotify Favourites
       </p>
-      <form id="searchForm">
+      <form method="post" onSubmit={handleSubmit}>
         <label className="searchLabel"> Genre:
-          <select id="genre" name="genrelist" form="searchform">
+          <select id="genre" name="genre">
             <option value="AllSongs">All</option>
             <option value="rock">Rock</option>
             <option value="pop">Pop</option>
@@ -21,13 +32,14 @@ export default function NavBar({handler}) {
           </select>
         </label>
         <label className="searchLabel"> Decade:
-          <select id="genre" name="genrelist" form="searchform">
+          <select id="genre" name="decade">
             <option value="AllYears">All</option>
             <option value="1980">1980</option>
             <option value="1990">1990</option>
             <option value="2000">2000</option>
           </select>
         </label>
+        <button type="submit">Discover</button>
       </form>
     </nav>
   );
