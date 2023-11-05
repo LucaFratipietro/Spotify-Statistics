516d1f5d3cdc6eb063de357a97e49f3862742d5e
lucafratipietro <luca.fratipietro@dawsoncollege.qc.ca>
2023-11-05 14:46:22 -0500

    Add basic navbar with styling

---
+++ client/src/components/Navbar.js
@@ -0,0 +1,33 @@
+import '../styling/Navbar.css';
+
+export default function NavBar({handler}) {
+
+  const handleChange = (event) => handler();
+
+  return (
+    <nav>
+      <h1>
+      Spotify Favourites
+      </h1>
+      <form id="searchForm">
+        <label> Genre:
+          <select id="genre" name="genrelist" form="searchform">
+            <option value="AllSongs">All</option>
+            <option value="rock">Rock</option>
+            <option value="pop">Pop</option>
+            <option value="rnb">RnB</option>
+          </select>
+        </label>
+        <label> Decade:
+          <select id="genre" name="genrelist" form="searchform">
+            <option value="AllYears">All</option>
+            <option value="1980">1980</option>
+            <option value="1990">1990</option>
+            <option value="2000">2000</option>
+          </select>
+        </label>
+      </form>
+    </nav>
+  );
+
+}
