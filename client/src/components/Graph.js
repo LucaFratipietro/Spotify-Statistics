791e09639e6af3668ceaf17a23f3f89580e228a4
Adriano D'Alonzo <adrianodalonzo@gmail.com>
2023-11-05 21:49:00 -0500

    Remove excessive comments

--- client/src/components/Graph.js
+++ client/src/components/Graph.js
@@ -7,32 +7,32 @@ export default function Graph({ songs }) {
 
   let dataset = [];
 
-  if(songs.length !== 0) {
-    // get unique genres
-    const genres = utils.separateGenres(songs);
-
-    dataset = genres.map(genre => {
-      return {
-        // set label to genre
-        label: genre.charAt(0).toUpperCase() + genre.slice(1),
-        // map each decade
-        data: utils.decades.map(decade => {
-          // get songs from current decade
-          const songsInDecade = utils.separateSongsToDecades(songs)[decade];
-          // get all songs from current genre and decade
-          const songsInDecadeAndGenre = songsInDecade.filter(song => song.Genre === genre);
-          // calculate total popularity of each song from current genre and decade
-          const totalPopularity = songsInDecadeAndGenre.reduce((sum, song) => {
-            return sum + song.popularity;
-          }, 0);
-          // calculate average popularity from songs
-          const averagePopularity = totalPopularity / songsInDecadeAndGenre.length;
-          // return average popularity
-          return averagePopularity;
-        })
-      };
-    });
-  }
+  /**
+   * Generates dataset for each data in the chart for songs.
+   * Uses unique genres to create a label
+   * Maps each decade to include the average popularity for each song
+   * in that genre.
+   */
+  (function generateDataset() {
+    if(songs.length !== 0) {
+      const genres = utils.separateGenres(songs);
+  
+      dataset = genres.map(genre => {
+        return {
+          label: genre.charAt(0).toUpperCase() + genre.slice(1),
+          data: utils.decades.map(decade => {
+            const songsInDecade = utils.separateSongsToDecades(songs)[decade];
+            const songsInDecadeAndGenre = songsInDecade.filter(song => song.Genre === genre);
+            const totalPopularity = songsInDecadeAndGenre.reduce((sum, song) => {
+              return sum + song.popularity;
+            }, 0);
+            const averagePopularity = totalPopularity / songsInDecadeAndGenre.length;
+            return averagePopularity;
+          })
+        };
+      });
+    }
+  })();
 
   /**
    * Function to either display graph or loading message
